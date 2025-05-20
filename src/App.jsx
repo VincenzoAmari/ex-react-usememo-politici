0ecb120c9c553ded385e1586b6c022aae3cab579
VincenzoAmari <vincenzo.am99@gmail.com>
2025-05-20 14:57:12 +0200

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [politici, setPolitici] = useState([]);
+  const [ricerca, setRicerca] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:3333/politicians")
@@ -10,13 +11,27 @@ function App() {
       .catch((error) => console.error(error));
   }, []);
 
-  console.log(politici);
+  const filtroPolitici = politici.filter((politico) => {
+    const nelNome = politico.name
+      .toLowerCase()
+      .includes(ricerca.toLocaleLowerCase());
+    const nellaBiografia = politico.biography
+      .toLowerCase()
+      .includes(ricerca.toLocaleLowerCase());
+    return nelNome || nellaBiografia;
+  });
 
   return (
     <div>
       <h1>lista politici</h1>
+      <input
+        type="text"
+        placeholder="cerca per nome o biografia"
+        value={ricerca}
+        onChange={(e) => setRicerca(e.target.value)}
+      />
       <div className="listaPolitici">
-        {politici.map((politico) => (
+        {filtroPolitici.map((politico) => (
           <div className="card" key={politico.id}>
             <img src={politico.image} alt={politico.name} />
             <h2> {politico.name} </h2>

aea79d4ad9774fcd66097305036a5ec27889c89b
VincenzoAmari <vincenzo.am99@gmail.com>
2025-05-20 15:22:55 +0200

    import errato

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo, memo } from "react";
 
 const PoliticoCard = memo(function PoliticoCard({ politico }) {
   return (
